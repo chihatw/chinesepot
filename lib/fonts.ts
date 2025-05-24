c7efebdf8f8ca2ed4d682480e52055fd869b8e71
chihatw <chiha.tw@gmail.com>
2025-05-24 21:04:20 +0900

    chore: 依存パッケージのダウングレード: nextを15→14、reactを19→18、types/nodeを22→20、tailwindcssを4→3に変更

--- lib/fonts.ts
+++ lib/fonts.ts
@@ -1,25 +1,4 @@
-import {
-  JetBrains_Mono as FontMono,
-  Noto_Serif_TC as FontPinyin,
-  Noto_Sans_HK as FontSans,
-  Zen_Maru_Gothic,
-} from 'next/font/google';
-
-export const fontSans = FontSans({
-  weight: ['100', '300', '400', '700', '900'],
-  subsets: ['latin'],
-  variable: '--font-sans',
-});
-
-export const fontMono = FontMono({
-  subsets: ['latin'],
-  variable: '--font-mono',
-});
-
-export const fontZenMaruGothic = Zen_Maru_Gothic({
-  subsets: ['latin'],
-  weight: ['400', '300'],
-});
+import { Noto_Serif_TC as FontPinyin } from 'next/font/google';
 
 export const fontPinyin = FontPinyin({
   subsets: ['latin'],
--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  logging: {
-    fetches: {
-      fullUrl: false,
-    },
-  },
+  /* config options here */
 };
 
 export default nextConfig;
